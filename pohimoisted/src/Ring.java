cbd5139508e4ca5ff5e2dc8b802623e111d7257a
Truija99 <Suema1234>
2018-11-15 11:11:24 +0200

    Täiendatud Ring tüübi kirjeldus konstruktoriga, mis võimaldab luua ringi raadiusega 0.0

--- pohimoisted/src/Ring.java
+++ pohimoisted/src/Ring.java
@@ -3,6 +3,10 @@ public class Ring {
     public Double x; //kordinaat x ja y
     public Double y;
 
+    public Ring() {
+        this.raadius =  0.0; //double tüüpi, sellepärast 2 0-i
+    }
+
     public void pindala() {
         Double s = 3.14 * raadius * raadius;
         System.out.println(s);
