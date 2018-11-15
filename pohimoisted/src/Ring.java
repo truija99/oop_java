4a7bd029a5d60611927bb7725ac280a44b41ce54
Truija99 <Suema1234>
2018-11-15 11:02:41 +0200

    Ring tüüpi loodud kirjeldus ja rakendatud konkreetse Ring tüüpi objekti loomisel

---
+++ pohimoisted/src/Ring.java
@@ -0,0 +1,11 @@
+public class Ring {
+    public Double raadius;
+    public Double x; //kordinaat x ja y
+    public Double y;
+
+    public void pindala() {
+        Double s = 3.14 * raadius * raadius;
+        System.out.println(s);
+    }
+
+}
---
+++ pohimoisted/src/RingTest.java
@@ -0,0 +1,7 @@
+public class RingTest {
+    public static void main(String[] args) {
+        Ring minuRing = new Ring ();
+        minuRing.raadius = 2.7;
+        minuRing. pindala();
+    }
+}
