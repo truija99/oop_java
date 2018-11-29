591e97bdf9d999b839068e413c3e26a0690ad081
Truija99 <Suema1234>
2018-11-29 10:50:22 +0200

    Täiendati Printer konstruktori tooneri üle täitmise kaitsega

--- pohimoisted/src/Printer.java
+++ pohimoisted/src/Printer.java
@@ -4,7 +4,13 @@ public class Printer {
     private Boolean kahepoolne;
 
     public Printer(Integer tooneriTase, Boolean kahepoolne) {
-        this.tooneriTase = tooneriTase;
+        if(tooneriTase >= 0 & tooneriTase <= 100) {
+            this.tooneriTase = tooneriTase;  //pidime selle muutma ära, sest muidu läks tooneri tase üle 100
+        } else if(tooneriTase > 100){
+            this.tooneriTase = 100;
+        } else {
+            this.tooneriTase = 0;
+        }
         this.kahepoolne = kahepoolne;
         this.paberPrintimiseks = 100;
     }
--- pohimoisted/src/PrinterTest.java
+++ pohimoisted/src/PrinterTest.java
@@ -1,6 +1,6 @@
 public class PrinterTest {
     public static void main(String[] args) {
-        Printer printer1 = new Printer (50, true); //ei ole kahepoolne
+        Printer printer1 = new Printer (150, true); //ei ole kahepoolne
         printer1.lisaTooner(15);
         System.out.println("Printeri tooneri tase on " + printer1.getTooneriTase() + "%"); //get tooneritase jaoks pidin looma setteri
 
