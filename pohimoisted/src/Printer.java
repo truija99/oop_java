9b554b27fa4112e0a97aca23870962205e2fe8e3
Truija99 <Suema1234>
2018-11-29 09:57:17 +0200

    Täiendati Printer tooneri tase muutmisega

--- pohimoisted/src/Printer.java
+++ pohimoisted/src/Printer.java
@@ -1,12 +1,29 @@
 public class Printer {
-    public Integer tooneriTase;
-    public Integer paberPrintimiseks;
-    public Boolean kahepoolne;
+    private Integer tooneriTase;
+    private Integer paberPrintimiseks;
+    private Boolean kahepoolne;
 
     public Printer(Integer tooneriTase, Boolean kahepoolne) {
         this.tooneriTase = tooneriTase;
         this.kahepoolne = kahepoolne;
-        this.paberPrintimiseks = 100; //100 lehte printimiseks
+        this.paberPrintimiseks = 100;
+    }
+
+
+    public Integer getTooneriTase() {  //see läks PrinterTest sout asja
+        return tooneriTase;
+    }
+
+    public Integer lisaTooner(Integer toonerJuurde) {       //peame kontrollima tooneri taset
+        if (tooneriTase >=  0 & this.tooneriTase <= 100) {
+            if(this.tooneriTase + toonerJuurde > 100) {
+                return -1;
+            }
+            this.tooneriTase = this.tooneriTase + toonerJuurde;
+            return this.tooneriTase;
+        } else {
+            return -1;
+        }
     }
 
 }
--- pohimoisted/src/PrinterTest.java
+++ pohimoisted/src/PrinterTest.java
@@ -1,13 +1,10 @@
 public class PrinterTest {
     public static void main(String[] args) {
         Printer printer1 = new Printer (50, false); //ei ole kahepoolne
-        System.out.println("Printeris on " + printer1.paberPrintimiseks + " lehti printimiseks");
-        System.out.println("Printeri tooneri tase on " + printer1.tooneriTase + "%");
+        printer1.lisaTooner(15);
+        System.out.println("Printeri tooneri tase on " + printer1.getTooneriTase() + "%"); //get tooneritase jaoks pidin looma setteri
 
-        // muudame printeri seisundi
-        printer1.paberPrintimiseks = 45; //45 lehte juures
-        printer1.tooneriTase = 120;
-        System.out.println("Printeri tooneri tase on " + printer1.tooneriTase + "%");
-        System.out.println("Printeris on " + printer1.paberPrintimiseks + " lehti printimiseks");
+        printer1.lisaTooner(50);
+        System.out.println("Printeri tooneri tase on " + printer1.getTooneriTase() + "%");
     }
 }
